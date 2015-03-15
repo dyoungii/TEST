81da42a3942c057ab0c8f201a7deff762052f4cf
NKAKoin <dyoungii@hotmail.com>
2015-03-15 04:55:10 -0500

    mac qt5

--- src/qt/macdockiconhandler.mm
+++ src/qt/macdockiconhandler.mm
@@ -1,14 +1,20 @@
+// Copyright (c) 2011-2013 The Bitcoin Core developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "macdockiconhandler.h"
 
-#include <QMenu>
-#include <QWidget>
-
-#include <QTemporaryFile>
 #include <QImageWriter>
+#include <QMenu>
+#include <QTemporaryFile>
+#include <QWidget>
 
 #undef slots
 #include <Cocoa/Cocoa.h>
+
+#if QT_VERSION < 0x050000
+extern void qt_mac_set_dock_menu(QMenu *);
+#endif
 
 @interface DockIconClickEventHandler : NSObject
 {
@@ -39,8 +45,9 @@
     Q_UNUSED(event)
     Q_UNUSED(replyEvent)
 
-    if (dockIconHandler)
+    if (dockIconHandler) {
         dockIconHandler->handleDockIconClickEvent();
+    }
 }
 
 @end
@@ -48,18 +55,28 @@
 MacDockIconHandler::MacDockIconHandler() : QObject()
 {
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-    this->m_dockIconClickEventHandler = [[DockIconClickEventHandler alloc] initWithDockIconHandler:this];
 
+    this->m_dockIconClickEventHandler = [[DockIconClickEventHandler alloc] initWithDockIconHandler:this];
     this->m_dummyWidget = new QWidget();
     this->m_dockMenu = new QMenu(this->m_dummyWidget);
-    
+    this->setMainWindow(NULL);
+#if QT_VERSION < 0x050000
+    qt_mac_set_dock_menu(this->m_dockMenu);
+#elif QT_VERSION >= 0x050200
+    this->m_dockMenu->setAsDockMenu();
+#endif
     [pool release];
+}
+
+void MacDockIconHandler::setMainWindow(QMainWindow *window) {
+    this->mainWindow = window;
 }
 
 MacDockIconHandler::~MacDockIconHandler()
 {
     [this->m_dockIconClickEventHandler release];
     delete this->m_dummyWidget;
+    this->setMainWindow(NULL);
 }
 
 QMenu *MacDockIconHandler::dockMenu()
@@ -74,25 +91,26 @@ void MacDockIconHandler::setIcon(const QIcon &icon)
     if (icon.isNull())
         image = [[NSImage imageNamed:@"NSApplicationIcon"] retain];
     else {
-		// generate NSImage from QIcon and use this as dock icon.
+        // generate NSImage from QIcon and use this as dock icon.
         QSize size = icon.actualSize(QSize(128, 128));
         QPixmap pixmap = icon.pixmap(size);
-        
-		// write temp file hack (could also be done through QIODevice [memory])
-		QTemporaryFile notificationIconFile;
-		if (!pixmap.isNull() && notificationIconFile.open()) {
-			QImageWriter writer(&notificationIconFile, "PNG");
-			if (writer.write(pixmap.toImage())) {
-				const char *cString = notificationIconFile.fileName().toUtf8().data();
-				NSString *macString = [NSString stringWithCString:cString encoding:NSUTF8StringEncoding];
-				image =  [[NSImage alloc] initWithContentsOfFile:macString];
-			}
-		}
-		if(!image) {
-				// if testnet image could not be created, load std. app icon
-				// if testnet image could not be created, load std. app icon
-		}
-   }
+
+        // write temp file hack (could also be done through QIODevice [memory])
+        QTemporaryFile notificationIconFile;
+        if (!pixmap.isNull() && notificationIconFile.open()) {
+            QImageWriter writer(&notificationIconFile, "PNG");
+            if (writer.write(pixmap.toImage())) {
+                const char *cString = notificationIconFile.fileName().toUtf8().data();
+                NSString *macString = [NSString stringWithCString:cString encoding:NSUTF8StringEncoding];
+                image =  [[NSImage alloc] initWithContentsOfFile:macString];
+            }
+        }
+
+        if(!image) {
+            // if testnet image could not be created, load std. app icon
+            image = [[NSImage imageNamed:@"NSApplicationIcon"] retain];
+        }
+    }
 
     [NSApp setApplicationIconImage:image];
     [image release];
@@ -109,5 +127,11 @@ MacDockIconHandler *MacDockIconHandler::instance()
 
 void MacDockIconHandler::handleDockIconClickEvent()
 {
+    if (this->mainWindow)
+    {
+        this->mainWindow->activateWindow();
+        this->mainWindow->show();
+    }
+
     emit this->dockIconClicked();
 }
